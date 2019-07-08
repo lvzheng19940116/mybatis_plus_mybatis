493811c81438c129025e5d277a7d7736cee99ffe
lvzheng <lvzheng@samsundot.com>
2019-07-08 10:37:41 +0800

    Mybatis-Plus

---
+++ src/main/java/com/mycat/mybatis/plus/MyBatisPlusGenerator.java
@@ -0,0 +1,95 @@
+package com.mycat.mybatis.plus;
+
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+import java.sql.SQLException;
+
+/**
+ * 以动手实践为荣,以只看不练为耻.
+ * 以打印日志为荣,以出错不报为耻.
+ * 以局部变量为荣,以全局变量为耻.
+ * 以单元测试为荣,以手工测试为耻.
+ * 以代码重用为荣,以复制粘贴为耻.
+ * 以多态应用为荣,以分支判断为耻.
+ * 以定义常量为荣,以魔法数字为耻.
+ * 以总结思考为荣,以不求甚解为耻.
+ *
+ * @author LvZheng
+ * 创建时间：2019/6/20 4:15 PM
+ */
+public class MyBatisPlusGenerator {
+
+    public static void main(String[] args) throws SQLException {
+        String projectPath = System.getProperty("user.dir");
+        //1. 全局配置
+        GlobalConfig config = new GlobalConfig();
+        config.setActiveRecord(true) // 是否支持AR模式
+                .setAuthor("LvZheng") // 作者
+                .setOutputDir(projectPath + "/src/main/java")
+                .setFileOverride(true)  // 文件覆盖
+                .setIdType(IdType.AUTO) // 主键策略
+                .setServiceName("%sService")  // 设置生成的service接口的名字的首字母是否为I
+                // IEmployeeService
+                .setMapperName("%sRepository")//jpa需要 如果使用MP  注释该代码
+                .setBaseResultMap(true)//生成基本的resultMap
+                .setBaseColumnList(true);//生成基本的SQL片段
+
+        //2. 数据源配置
+        DataSourceConfig dsConfig = new DataSourceConfig();
+
+//        username: ordercenter_testuser
+//        password: abcd-1234
+//        url: jdbc:mysql://10.122.30.102:3306/bank_collection?characterEncoding=utf-8&useSSL=false
+
+        dsConfig.setDbType(DbType.MYSQL)  // 设置数据库类型
+                .setDriverName("com.mysql.jdbc.Driver")
+                .setUrl("jdbc:mysql://10.122.30.102:3306/bank_collection?characterEncoding=utf-8&useSSL=false")
+                .setUsername("ordercenter_testuser")
+                .setPassword("abcd-1234");
+//        dsConfig.setDbType(DbType.MYSQL)  // 设置数据库类型
+//                .setDriverName("com.mysql.jdbc.Driver")
+//                .setUrl("jdbc:mysql://localhost:3306/fly")
+//                .setUsername("root")
+//                .setPassword("root");
+
+        //3. 策略配置globalConfiguration中
+        StrategyConfig stConfig = new StrategyConfig();
+        stConfig.setCapitalMode(true) //全局大写命名
+//                    .setDbColumnUnderline(true)  // 指定表名 字段名是否使用下划线
+                .setNaming(NamingStrategy.underline_to_camel) // 数据库表映射到实体的命名策略
+                //.setTablePrefix("tbl_")
+                .setInclude(new String[] { "base_gab","base_kab","base_kab_back", "base_gab_back"})
+        ;  // 生成的表
+
+        //4. 包名策略配置
+        PackageConfig pkConfig = new PackageConfig();
+        pkConfig.setParent("com.mycat.mybatis.plus")
+                .setMapper("mapper")//dao
+                .setService("service")//servcie
+                .setController("controller")//controller
+                .setEntity("entity")
+                .setXml("mapper");//mapper.xml
+
+        //5. 整合配置
+        AutoGenerator ag = new AutoGenerator();
+        ag.setGlobalConfig(config)
+                .setDataSource(dsConfig)
+                .setStrategy(stConfig)
+                .setPackageInfo(pkConfig);
+
+        //6. 执行
+        ag.execute();
+    }
+
+}
+
+
+
